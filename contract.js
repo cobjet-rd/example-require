d9db9b7898d190de8f6033639ad01d5c74a554ae
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-10-07 13:51:35 -0700

    [TASK] Include more complex require operations

--- contract.js
+++ contract.js
@@ -5,3 +5,8 @@ console.log(example.example);
 var myExample = require('myexample');
 
 console.log(myExample.bar);
+
+var foo = require('./lib/1').foo;
+var bar = require('./lib/other/2').bar;
+
+console.log(foo + ' + ' + bar + ' = ' + (foo + bar));
